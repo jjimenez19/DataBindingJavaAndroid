0d6ad282cceb03a22b125915531f7aadd4b6d261
Jose <josemanueljimenezpiqueras@gmail.com>
2020-04-03 11:42:20 +0200

    Binding de Nombre y Apellidos

--- app/src/main/java/com/example/databindingjavaandroid/MainActivity.java
+++ app/src/main/java/com/example/databindingjavaandroid/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.databindingjavaandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.databindingjavaandroid.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    //binding
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        mBinding.setLastName("Jimenez Piqueras");
+        mBinding.setName("José Manuel");
     }
 }
